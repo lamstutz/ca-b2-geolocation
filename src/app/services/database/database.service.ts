bf7fbc46f6cbb3a00175fdfb5f3929cdec418e1d
louis amstutz <louis.amstutz@gmail.com>
2020-01-10 13:43:09 +0100

    remove unused import

--- src/app/services/database/database.service.ts
+++ src/app/services/database/database.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import { promise } from 'protractor';
 
 @Injectable({
   providedIn: 'root'
